67251cc3ecb207e1e3db3c6580c17d2cea6e1f5d
AlexMelnik2003 <alexz.melnik.02@gmail.com>
2024-05-02 19:32:19 +0600

    ggggggg

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('api/glava/', ProductAPIView1.as_view()),
     path('api/token1/', UserRegistrationView.as_view(), name='token_obtain2_pasir'),
     path('api/token12/', UserRegistrationView1.as_view(), name='token_obtain2_pasir'),
-    path('api/grait1/', UserRegistrationView2.as_view(), name='token_obtain2_pasir'),
+    path('api/grait14/', UserRegistrationView2.as_view(), name='token_obtain2_pasir'),
 ]
